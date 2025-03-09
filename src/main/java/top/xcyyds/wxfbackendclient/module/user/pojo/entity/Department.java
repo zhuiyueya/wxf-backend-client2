d509534eec67b40dbfc6605a0f13869e943232a9
钟耀 戴 <3436265070@qq.com>
2025-03-09 20:36:39 +0800

    feat(user):创建user的entity类
    - 新建Department类
    - 新建Major类
    - 新建UserAuth类
    - 新建User类
    - 新建UserSchoolEnrollInfo类

---
+++ src/main/java/top/xcyyds/wxfbackendclient/module/user/pojo/entity/Department.java
@@ -0,0 +1,11 @@
+package top.xcyyds.wxfbackendclient.module.user.pojo.entity;
+
+/**
+ * @Author: chasemoon
+ * @CreateTime: 2025-03-09
+ * @Description:
+ * @Version:
+ */
+
+public class Department {
+}
