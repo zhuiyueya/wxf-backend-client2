feb6c9d2f1c0b0797d4277733efb2e066fc1bf4b
钟耀 戴 <3436265070@qq.com>
2025-03-09 17:37:21 +0800

    build(All project):初始化项目

---
+++ src/main/java/top/xcyyds/wxfbackendclient/WxfBackendClientApplication.java
@@ -0,0 +1,13 @@
+package top.xcyyds.wxfbackendclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxfBackendClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxfBackendClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/xcyyds/wxfbackendclient/WxfBackendClientApplicationTests.java
@@ -0,0 +1,13 @@
+package top.xcyyds.wxfbackendclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WxfBackendClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
