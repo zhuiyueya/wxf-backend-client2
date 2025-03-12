bdb455a027cdcd63f094d5aa991ee41c7efb05c6
钟耀 戴 <3436265070@qq.com>
2025-03-12 08:44:24 +0800

    feat(common):增加统一返回前端类 - 包含状态码，状态信息，返回数据三个字段 - 操作成功返回函数 - 操作失败返回函数（to do）

---
+++ src/main/java/top/xcyyds/wxfbackendclient/common/Result.java
@@ -0,0 +1,60 @@
+package top.xcyyds.wxfbackendclient.common;
+
+import lombok.Data;
+
+/**
+ * @Author: chasemoon
+ * @CreateTime: 2025-03-09
+ * @Description:统一返回类，返回到前端
+ * @Version:
+ */
+@Data
+public class Result<T> {
+    /*
+     *状态码
+     */
+    private int code;
+    /*
+     *成功/失败信息
+     */
+    private String msg;
+    /*
+     *返回数据
+     */
+    private T data;
+
+    public Result(int code, String msg, T data) {
+        this.code = code;
+        this.msg = msg;
+        this.data = data;
+    }
+
+    public Result() {
+    }
+
+    /*
+     * @Description: 操作执行成功的返回函数
+     * @param T
+     * @return: Result<T>
+     * @Author:  chasemoon
+     * @date:  2025/3/12 08:41
+     */
+    public static <T> Result<T>success(T data){
+        Result<T>result =new Result<>();
+        result.setCode(200);
+        result.setMsg("success");
+        result.setData(data);
+        return result;
+    }
+    /*
+     * @Description: 操作执行失败的返回函数
+     * @param T
+     * @return: Result<T>
+     * @Author:  chasemoon
+     * @date:  2025/3/12 08:41
+     */
+    public static <T> Result<T>error(){
+        // to do 。。。。。。
+        return null;
+    }
+}
