0b6bb38776b960a2a378ad280339f2f9044ce311
钟耀 戴 <3436265070@qq.com>
2025-03-14 16:53:58 +0800

    feat(auth):添加`@JsonProperty`注解并手动定义getter和setter  - 为`isNewUser`字段添加了`@JsonProperty`注解，同时移除了`@lombok.Data`注解，并手动实现了getter和setter方法，以避免字段名is开头带来的解析异常：后端字段名是isNewUser，但前端接收到的字段名变为了newUser

--- src/main/java/top/xcyyds/wxfbackendclient/module/auth/pojo/dto/LoginResponse.java
+++ src/main/java/top/xcyyds/wxfbackendclient/module/auth/pojo/dto/LoginResponse.java
@@ -1,5 +1,8 @@
 package top.xcyyds.wxfbackendclient.module.auth.pojo.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
 /**
  * @Author: chasemoon
  * @CreateTime: 2025-03-09
@@ -7,14 +10,35 @@ package top.xcyyds.wxfbackendclient.module.auth.pojo.dto;
  * @Version:
  */
 
-@lombok.Data
+
 public class LoginResponse {
-    /**
-     * 是否为新注册用户（用于前端引导完善信息）
-     */
-    private boolean isNewUser;
+
     /**
      * 登录令牌（如JWT）,在后续请求中附加到header携带
      */
     private String token;
+
+    /**
+     * 是否为新注册用户（用于前端引导完善信息）
+     */
+
+    private boolean isNewUser;
+
+    @JsonProperty(value="isNewUser")
+    public boolean isNewUser() {
+        return isNewUser;
+    }
+
+    public void setIsNewUser(boolean newUser) {
+        isNewUser = newUser;
+    }
+
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
