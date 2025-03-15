5d30f162cfb098e63eab5fb6293d387f419788e0
钟耀 戴 <3436265070@qq.com>
2025-03-15 20:15:48 +0800

    feat（util):添加JwtUtil工具类和相关配置
    - 新增JwtUtil类用于生成和验证token
    - 在pom.xml中添加了JJWT库依赖
    - 在application-pro.yml中增加JWT的密钥和过期时间的配置选项

---
+++ src/main/java/top/xcyyds/wxfbackendclient/util/JwtUtil.java
@@ -0,0 +1,54 @@
+package top.xcyyds.wxfbackendclient.util;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+
+/**
+ * @Author: chasemoon
+ * @CreateTime: 2025-03-15
+ * @Description:jwt组件，用来生成和验证token
+ * @Version:v1
+ */
+@Slf4j
+@Component
+public class JwtUtil {
+    @Value("${jwt.secret}")
+    private  String secret;// 用于存储 JWT 签名密钥的私有字符串字段。
+    @Value("${jwt.expiration}")
+    private  long expiration;// 存储 JWT 过期时间的私有长整型字段。
+
+    // 使用给定的密钥字节创建 HMAC SHA-256 密钥。
+    public  SecretKey getSigningKey(){
+        return Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
+    /*
+     * @Description: 生成token
+     * @param subject：要融合到token里的信息
+     * @return: String
+     * @Author:  chasemoon
+     * @date:  2025/3/15 20:13
+     */
+    public  String generateToken(String subject) {
+        Date now=new Date();
+        Date expireDate=new Date(now.getTime()+expiration);// 计算出令牌的到期时间。
+
+        return Jwts.builder()// 开始构建 JWT 对象
+                .setSubject(subject)// 设置 JWT 的主体信息
+                .setIssuedAt(now)// 设置 JWT 的签发时间
+                .setExpiration(expireDate)// 设置 JWT 的过期时间
+                .signWith(getSigningKey())// 使用之前定义的签名密钥对 JWT 进行签名。
+                .compact();// 将整个 JWT 编码为紧凑形式的字符串并返回。
+    }
+
+}
+
