16b2f74fbc40f2bc5899ad4999592b3243900b25
钟耀 戴 <3436265070@qq.com>
2025-03-15 16:34:20 +0800

    feat(auth):实现抽象登录策略方法
    - 实现了 `AbstractLoginStrategy` 中的 `login` 方法
    - 调用子类认证函数进行认证
    - 调用generateToken生成token
    - 创建LoginResponse并返回

--- src/main/java/top/xcyyds/wxfbackendclient/module/auth/service/AbstractLoginStrategy.java
+++ src/main/java/top/xcyyds/wxfbackendclient/module/auth/service/AbstractLoginStrategy.java
@@ -1,7 +1,12 @@
 package top.xcyyds.wxfbackendclient.module.auth.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.LoginRequest;
 import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.LoginResponse;
+import top.xcyyds.wxfbackendclient.module.auth.service.impl.AuthService;
 import top.xcyyds.wxfbackendclient.module.user.pojo.entity.User;
 import top.xcyyds.wxfbackendclient.module.user.pojo.enums.LoginType;
 
@@ -12,16 +17,30 @@ import top.xcyyds.wxfbackendclient.module.user.pojo.enums.LoginType;
  * @Version:
  */
 
+@Component
+@Slf4j
 public abstract class AbstractLoginStrategy implements LoginStrategy {
+
 
     @Override
     public LoginResponse login(LoginRequest loginRequest) {
-        return null;
+        log.info("top/xcyyds/wxfbackendclient/module/auth/service/AbstractLoginStrategy:login start>>>{}",loginRequest);
+        User user=authenticate(loginRequest);
+        LoginResponse response=new LoginResponse();
+        response.setIsNewUser(false);
+        if(user==null){
+            //register 待定，应该在子strategy类里处理，才能根据不同的登录方式处理
+            response.setIsNewUser(true);
+        }
+        response.setToken(generateToken(user.getPublicId()));
+
+        return response;
     }
     public abstract User authenticate(LoginRequest loginRequest);
 
     public String generateToken(String subject) {
-        return null;
+
+        return subject;
     }
 
     @Override
