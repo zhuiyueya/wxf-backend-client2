7d4dda811b33e1ff7230e6be23d01caed7203f7b
钟耀 戴 <3436265070@qq.com>
2025-03-15 16:27:20 +0800

    feat(auth):添加通过code获取openID的函数
    - 实现了通过微信code获取openid的方法，
    - 更新了对登录类型的判断以支持微信登录。

--- src/main/java/top/xcyyds/wxfbackendclient/module/auth/service/impl/WechatLoginStrategy.java
+++ src/main/java/top/xcyyds/wxfbackendclient/module/auth/service/impl/WechatLoginStrategy.java
@@ -1,5 +1,12 @@
 package top.xcyyds.wxfbackendclient.module.auth.service.impl;
 
+import ch.qos.logback.core.joran.spi.HttpUtil;
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import me.chanjar.weixin.common.error.WxErrorException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.LoginRequest;
 import top.xcyyds.wxfbackendclient.module.auth.service.AbstractLoginStrategy;
 import top.xcyyds.wxfbackendclient.module.user.pojo.entity.User;
@@ -12,15 +19,28 @@ import top.xcyyds.wxfbackendclient.module.user.pojo.enums.LoginType;
  * @Version:
  */
 
+@Component
 public class WechatLoginStrategy extends AbstractLoginStrategy {
 
+    @Autowired
+    private WxMaService wxMaService;
     @Override
     public User authenticate(LoginRequest loginRequest) {
         return null;
     }
 
+    private String getWechatOpenId(String code){
+        try{
+            WxMaJscode2SessionResult session=wxMaService.jsCode2SessionInfo(code);
+            return session.getOpenid();
+        }catch(WxErrorException e){
+            throw new RuntimeException("微信登录失败:"+e.getMessage());
+        }
+
+    }
+
     @Override
     public boolean canSupportedLoginType(LoginType loginType) {
-        return false;
+        return loginType.equals(LoginType.WECHAT_LOGIN);
     }
 }
