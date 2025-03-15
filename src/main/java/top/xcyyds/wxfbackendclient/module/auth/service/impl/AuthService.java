371dec8c78cee3d54f6a4231e6444706d8130ea1
钟耀 戴 <3436265070@qq.com>
2025-03-15 16:39:02 +0800

    feat(auth):实现 AuthService 类login方法
    添加必要的 Lombok 注解和 Spring 注解，引入新的登录策略接口，并实现登录方法以支持多种登录类型。

--- src/main/java/top/xcyyds/wxfbackendclient/module/auth/service/impl/AuthService.java
+++ src/main/java/top/xcyyds/wxfbackendclient/module/auth/service/impl/AuthService.java
@@ -1,10 +1,24 @@
 package top.xcyyds.wxfbackendclient.module.auth.service.impl;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.LoginRequest;
 import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.LoginResponse;
 import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.PhoneLoginRequest;
 import top.xcyyds.wxfbackendclient.module.auth.pojo.dto.WechatLoginRequest;
 import top.xcyyds.wxfbackendclient.module.auth.service.IAuthService;
+import top.xcyyds.wxfbackendclient.module.auth.service.LoginStrategy;
+import top.xcyyds.wxfbackendclient.module.user.pojo.entity.User;
+import top.xcyyds.wxfbackendclient.module.user.pojo.entity.UserAuth;
+import top.xcyyds.wxfbackendclient.module.user.pojo.enums.LoginType;
+import top.xcyyds.wxfbackendclient.util.IdGenerator;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
 
 /**
  * @Author: chasemoon
@@ -13,8 +27,15 @@ import top.xcyyds.wxfbackendclient.module.auth.service.IAuthService;
  * @Version:
  */
 
+
+@Slf4j
 @Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class AuthService implements IAuthService {
+
+
+    private final List<LoginStrategy> strategys;
+
 
     /*
      * @Description: 微信登录的Service,处理登录请求
@@ -24,9 +45,13 @@ public class AuthService implements IAuthService {
      * @date:  2025/3/12 08:53
      */
     @Override
-    public LoginResponse wechatLogin(WechatLoginRequest request) {
-        return null;
     public LoginResponse login(LoginRequest request) {
+        return
+                strategys.stream()
+                        .filter(strategy->strategy.canSupportedLoginType(request.getLoginType()))
+                        .findFirst()
+                        .map(strategy->strategy.login(request))
+                        .orElseThrow(()->new IllegalArgumentException("Unsupported login method :" + request.getClass().getName()));
     }
 
 
