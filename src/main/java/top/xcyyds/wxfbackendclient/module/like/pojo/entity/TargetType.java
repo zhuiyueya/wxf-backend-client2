980c369c8fe74c2c8d339f6ba30a40e0b8e1dee5
钟耀 戴 <3436265070@qq.com>
2025-03-29 15:49:55 +0800

    feat(like): 新增 TargetType 枚举类用于定义内容类型
    新增了 TargetType 枚举类，包含 COMMENT 和 POST 两种内容类型，并提供了 toValue 和 forValue 方法以支持值的转换和反序列化。

---
+++ src/main/java/top/xcyyds/wxfbackendclient/module/like/pojo/entity/TargetType.java
@@ -0,0 +1,33 @@
+package top.xcyyds.wxfbackendclient.module.like.pojo.entity;
+
+/**
+ * @Author: chasemoon
+ * @CreateTime: 2025-03-29
+ * @Description:
+ * @Version:
+ */
+
+
+import java.io.IOException;
+
+/**
+ * 附件所属的内容类型
+ */
+public enum TargetType {
+    COMMENT, POST;
+
+    public String toValue() {
+        switch (this) {
+            case COMMENT: return "COMMENT";
+            case POST: return "POST";
+        }
+        return null;
+    }
+
+    public static TargetType forValue(String value) throws IOException {
+        if (value.equals("COMMENT")) return COMMENT;
+        if (value.equals("POST")) return POST;
+        throw new IOException("Cannot deserialize TargetType");
+    }
+}
+
