684783e20a479e11fd3b8003cf817da374fa334f
钟耀 戴 <3436265070@qq.com>
2025-03-29 15:51:30 +0800

    feat(post): 添加SummaryPost数据传输对象
    在post模块中新增SummaryPost.java文件，定义了帖子的摘要信息数据传输对象，包含作者信息、收藏数量、内容摘要、发布时间、多媒体信息、点赞量、回复数量、分享数量等字段。

---
+++ src/main/java/top/xcyyds/wxfbackendclient/module/post/pojo/dto/SummaryPost.java
@@ -0,0 +1,73 @@
+package top.xcyyds.wxfbackendclient.module.post.pojo.dto;
+
+/**
+ * @Author: chasemoon
+ * @CreateTime: 2025-03-29
+ * @Description:
+ * @Version:
+ */
+
+import top.xcyyds.wxfbackendclient.module.mediaAttachment.pojo.dto.SummaryMediaAttachment;
+import top.xcyyds.wxfbackendclient.module.user.pojo.dto.SummaryAuthorInfo;
+
+import java.time.OffsetDateTime;
+import java.util.List;
+
+/**
+ * summaryPost
+ */
+@lombok.Data
+public class SummaryPost {
+    /**
+     * 作者信息
+     */
+    private SummaryAuthorInfo authorInfo;
+    /**
+     * 收藏数量
+     */
+    private double collectCount;
+    /**
+     * 内容摘要（Markdown格式）
+     */
+    private String content;
+    /**
+     * 发布时间（UTC时间）
+     */
+    private OffsetDateTime createTime;
+    /**
+     * 是否包含多媒体
+     */
+    private boolean hasMedia;
+    /**
+     * 用于获取帖子列表是判断此次获取是否获取了完整的信息，若是则在进入帖子详细页时可以不用请求
+     */
+    private boolean isComplete;
+    /**
+     * 点赞量
+     */
+    private double likeCount;
+    /**
+     * 帖子唯一标识
+     */
+    private double postId;
+    /**
+     * 回复数量
+     */
+    private double replyCount;
+    /**
+     * 分享数量
+     */
+    private double shareCount;
+    /**
+     * 媒体附件部分信息列表
+     */
+    private List<SummaryMediaAttachment> summaryMediaAttachment;
+    /**
+     * 附件总数
+     */
+    private double totalAttachments;
+    /**
+     * 浏览量
+     */
+    private double viewCount;
+}
