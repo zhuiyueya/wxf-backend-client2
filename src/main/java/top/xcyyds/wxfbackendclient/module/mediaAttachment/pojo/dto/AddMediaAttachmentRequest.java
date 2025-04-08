1d11ff92ff69d91fde00c687c62a2e0da01cb243
钟耀 戴 <3436265070@qq.com>
2025-04-08 09:10:25 +0800

    feat(mediaAttachment): 新增 AddMediaAttachmentRequest 请求对象
    - 新增了用于媒体附件添加的请求对象 AddMediaAttachmentRequest，包含 mediaAttachment、targetType、target 和 publicId 字段。

---
+++ src/main/java/top/xcyyds/wxfbackendclient/module/mediaAttachment/pojo/dto/AddMediaAttachmentRequest.java
@@ -0,0 +1,19 @@
+package top.xcyyds.wxfbackendclient.module.mediaAttachment.pojo.dto;
+
+import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
+import top.xcyyds.wxfbackendclient.module.like.pojo.entity.TargetType;
+
+/**
+ * @Author: chasemoon
+ * @CreateTime: 2025-04-07
+ * @Description:
+ * @Version:
+ */
+@Data
+public class AddMediaAttachmentRequest<T> {
+    private MultipartFile mediaAttachment;
+    private TargetType targetType;
+    private T target;
+    private String publicId;
+}
