afa2823ee0d73af338c125fc7b19ea078d507c71
钟耀 戴 <3436265070@qq.com>
2025-04-19 12:40:17 +0800

    feat(notification): 添加创建提醒和拉取提醒接口
    在 NotificationController 中新增了两个接口：
    1. `/create`：用于创建提醒。
    2. `/reminder/pull`：用于拉取提醒。
    
    同时调整了用户通知接口的返回值类型，从 `GetUserNotifyResponse` 修改为 `ListUserNotifysResponse`。

--- src/main/java/top/xcyyds/wxfbackendclient/module/notification/controller/NotificationController.java
+++ src/main/java/top/xcyyds/wxfbackendclient/module/notification/controller/NotificationController.java
@@ -3,15 +3,9 @@ package top.xcyyds.wxfbackendclient.module.notification.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import top.xcyyds.wxfbackendclient.common.Result;
-import top.xcyyds.wxfbackendclient.module.notification.pojo.dto.GetUserNotifyRequest;
-import top.xcyyds.wxfbackendclient.module.notification.pojo.dto.GetUserNotifyResponse;
-import top.xcyyds.wxfbackendclient.module.notification.pojo.dto.PullReminderRequest;
-import top.xcyyds.wxfbackendclient.module.notification.pojo.dto.PullReminderResponse;
+import top.xcyyds.wxfbackendclient.module.notification.pojo.dto.*;
 import top.xcyyds.wxfbackendclient.module.notification.service.INotificationService;
 
 /**
@@ -27,6 +21,7 @@ public class NotificationController {
     @Qualifier("NotificationService")
     private INotificationService notificationService;
 
+    @GetMapping("/reminder/pull")
     public Result<PullReminderResponse> pullReminder(Authentication authentication) {
         PullReminderRequest pullReminderRequest=new PullReminderRequest();
         pullReminderRequest.setUserPublicId(authentication.getPrincipal().toString());
@@ -34,10 +29,14 @@ public class NotificationController {
     }
 
     @PostMapping("/userNotify/list")
-    public Result<GetUserNotifyResponse> getUserNotify(Authentication authentication, @RequestBody GetUserNotifyRequest request) {
+    public Result<ListUserNotifysResponse> listUserNotify(Authentication authentication, @RequestBody GetUserNotifyRequest request) {
         request.setUserPublicId(authentication.getPrincipal().toString());
-        return Result.success(notificationService.getUserNotify(request));
+        return Result.success(notificationService.listUserNotify(request));
     }
-
+    @PostMapping("/create")
+    public Result<CreateReminderResponse> createNotification(Authentication authentication, @RequestBody CreateReminderRequest request) {
+        request.setSenderPublicId(authentication.getPrincipal().toString());
+        return Result.success(notificationService.createReminder(request));
+    }
 
 }
