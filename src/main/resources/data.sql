e973aeff7392829a6e3e72dce1fd5e040530bb6c
钟耀 戴 <3436265070@qq.com>
2025-04-19 12:36:38 +0800

    feat(notification): 添加通知相关的 SQL 数据及配置初始化
    在 application-pro.yml 中新增 sql.init.mode 和 jpa.defer-datasource-initialization 配置，确保启动时执行 SQL 文件并延迟数据源初始化。新增 data.sql 文件，插入通知相关操作类型的基础数据，包括点赞、评论和回复的操作描述与显示模板。

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO subscription_action_type (action_name, description, display_template)
+VALUES
+    ('LIKE', '点赞操作', '{{user}} 点赞了你的内容'),
+    ('COMMENT', '评论操作', '{{user}} 评论了你的内容'),
+    ('REPLY', '回复操作', '{{user}} 回复了你的内容')
+    ON DUPLICATE KEY UPDATE
+                         description = VALUES(description),
+                         display_template = VALUES(display_template);
